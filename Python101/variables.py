7259101ee156771ba474539e708c9c8743da507c
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-02-24 22:53:02 +0900

    変数のお勉強

---
+++ Python101/variables.py
@@ -0,0 +1,10 @@
+# 変数`life`に数を代入
+life = 100
+# 変数`score`に数を代入
+score = 0
+# 変数`jpb`に文字列を代入
+job = "勇者"
+# `print()`でそれぞれの値を出力
+print("lifeに代入した体力値は", life)
+print("scoreに代入した点数は", score)
+print("jobに代入した職業の文字列は", job)
