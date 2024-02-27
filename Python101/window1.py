6a40b834750c7dca6d053ec9c027a60e24bfe1b8
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-02-27 17:10:35 +0900

    window2

--- Python101/window1.py
+++ Python101/window1.py
@@ -1,3 +1,5 @@
-import tkinter
-root = tkinter.Tk()
-root.mainloop()
+import tkinter # tkinterをインポート
+root = tkinter.Tk() # ウィンドウのオブジェクトを作成
+root.mainloop() # ウィンドウの処理を開始
+
+# tkinterに携わるTk()がウィンドウを作るための命令
---
+++ Python101/window2.py
@@ -0,0 +1,5 @@
+import tkinter
+root = tkinter.Tk()
+root.geometry("800x400") # ウィンドウの大きさを調節
+root.title("タイトル") # ウィンドウのタイトルを指定
+root.mainloop()
