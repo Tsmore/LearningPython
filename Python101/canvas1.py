c942dc4b4b52cb6caea13896f5d00e895d3cae0f
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-02-27 17:19:20 +0900

    キャンバスを使う

---
+++ Python101/canvas1.py
@@ -0,0 +1,7 @@
+import tkinter
+root = tkinter.Tk()
+root.title("キャンバスに線を書く") # タイトルを指定
+cvs = tkinter.Canvas(width=600, height=400, bg="black") # キャンバスを用意
+cvs.create_line(0, 0, 500, 300, fill="red", width=5) # キャンバスに幅5の赤い線を引く
+cvs.pack() # キャンバスをウィンドウに配置
+root.mainloop()
